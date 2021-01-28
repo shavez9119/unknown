a6d7e92d7afc40688956ba441a8206ce0a513f28
shavez9119 <64725325+shavez9119@users.noreply.github.com>
2021-01-28 15:38:09 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,502 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		 <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+		<!-- responsive style sheet -->
+		<link rel="stylesheet" type="text/css" href="css/responsive.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" href="/js/script.js">
+		<link rel="preconnect" href="https://fonts.gstatic.com">
+		<script src='https://kit.fontawesome.com/a076d05399.js'></script>
+		<link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	</head>
+<style media="screen">
+.content p{
+	font-size:20px;
+}
+.pdfbtn{
+	background-color:darkorange;
+	border:none;
+	color:black;
+	padding:12px 40px;
+	transition: all .3s ease-in-out;
+	-webkit-transition: all .3s ease-in-out;
+	font-weight: bold;
+}
+.pdfbtn:hover{
+	background-color:black;
+	color:rgb(238, 238, 238);
+}
+p{
+	font-family: 'Roboto', sans-serif;
+}
+</style>
+	<body>
+<!--		<div class="top-header">
+			<div class="social-icon">
+			<a href="#"><i class="fa fa-facebook"></i></a>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="header">
+							<ul>
+								<li><a href="#">HOME</a></li>
+								<li><a href="#">UTKARSH</a></li>
+								<li><a href="#">CAFEBLE</a></li>
+								<li><a href="#">YOUR BIT</a></li>
+								<li><a href="#">CERTIFICATION</a></li>
+								<li><a href="#">CLIENT</a></li>
+								<li><a href="#">CONTACT</a></li>
+								</ul>
+						</div>
+					</div>
+				</div>
+			</div>    -->
+
+			<!--Slider-->
+
+
+			<div class="logo" data-aos="fade-down">
+				<img src="img/logo/logo3.png" alt="">
+			</div>
+
+				<nav>
+					<input type="checkbox" id="check">
+					<label for="check" class="checkbtn">
+						<i class="fas fa-bars"></i>
+					</label>
+
+					<ul data-aos="fade-down">
+						<li><a class="active" href="#">ABOUT</a></li>
+						<li><a href="#">UTKARSH</a></li>
+						<li><a href="#">CAFEBLE</a></li>
+						<li><a href="#">YOUR BIT</a></li>
+						<li><a href="#">CERTIFICATION</a></li>
+						<li><a href="#">CLIENTS</a></li>
+						<li><a href="#">CONTACT</a></li>
+					</ul>
+				</nav>
+
+
+<div id="slider" data-aos="fade-up">
+  <div class="slide" style="background:dodgerBlue;">
+    <img src="img/slider/1.png" >
+  </div>
+
+  <div class="slide" style="background:coral;">
+     <img src="img/slider/2.png" >
+  </div>
+
+  <div class="slide" style="background:crimson;">
+    <img src="img/slider/3.png" >
+  </div>
+
+  <div class="slide" style="background: #6edf10;">
+    <img src="img/slider/4.png" >
+  </div>
+
+  <!--Controlling arrows-->
+  <span class="controls" onclick="prevSlide(-1)" id="left-arrow"><i class="fas fa-angle-left" aria-hidden="true"></i>
+</span>
+  <span class="controls" id="right-arrow" onclick="nextSlide(1)"><i class="fas fa-angle-right" aria-hidden="true"></i>
+</span>
+</div>
+  <div id="dots-con">
+ <span class="dot"></span><span class="dot"></span><span class="dot"></span><span class="dot"></span>
+ </div>
+
+
+				<section>
+					<div class="container" data-aos="fade-down">
+						<div class="row">
+							<div class="col-md-12">
+							<div class=" col-md-12 text-center back">
+								<p>CREATING OPPORTUNITIES FOR DIFFERENTLY ABLE PEOPLE.</p>
+							</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="container">
+						<div class="row">
+							<div class="col-md-12">
+
+							<div class="col-md-3 cr" data-aos="fade-right">
+								<h1>105Cr.</h1>
+								<p>Total population of differently able</p>
+							</div>
+							<div class="col-md-3 cr cr2" data-aos="fade-up">
+								<h1>35Cr.</h1>
+								<p>Total population of differently able</p>
+							</div>
+							<div class="col-md-3 cr cr2" data-aos="fade-left">
+								<h1>1.5Cr.</h1>
+								<p>Total population of differently able</p>
+							</div>
+							<div class="col-md-3 cr" data-aos="fade-left">
+								<h1>1250Cr.</h1>
+								<p>Total population of differently able</p>
+							</div>
+
+							</div>
+						</div>
+					</div>
+
+				</section>
+
+
+				<section>
+				<div class="container">
+					<div class="row">
+							<hr class="hr">
+						<div class="col-md-12">
+							<div class="col-md-4" data-aos="fade-right">
+								<div style="clear:both; height:100px;"></div>
+								<h1 style="font-family: 'Fredoka One', cursive;"><b>UTKARSH</b></h1>
+								<h1 style="font-family: 'Fredoka One', cursive;"><span class="spane">ENABLE</span></h1>
+								<p style="font-size:18px;">EMPOWERING IMPAIRED</p>
+							</div>
+							<div class="col-md-8" data-aos="fade-left">
+								<img src="img/px-clouds-conifers-fir-trees-158672-origin.jpg" alt="" style="width:100%;" >
+							</div>
+
+						</div>
+					</div>
+				</div>
+				</section>
+
+				<section>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<div class="col-md-4 content" data-aos="fade-right">
+								<p>Assertively brand ethical meta-services after fully tested customer service fully Completely orchestrate intuitive these communities through superior markets.</p>
+							</div>
+							<div class="col-md-4 content" data-aos="fade-up">
+								<p>Assertively brand ethical meta-services after fully tested customer service fully Completely orchestrate intuitive these communities through superior markets.</p>
+							</div>
+							<div class="col-md-4 content" data-aos="fade-left">
+								<p>Assertively brand ethical meta-services after fully tested customer service fully Completely orchestrate intuitive these communities through superior markets.</p>
+							</div>
+						</div>
+					</div>
+					<hr class="hr">
+				</div>
+					</section>
+
+					<section>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<div class="col-md-4 content" data-aos="fade-right">
+								<img src="img/logo/logo3.png" style="height:150px; width:250px;" alt="">
+
+							</div>
+							<div class="col-md-4 content" data-aos="fade-up">
+								<p>Assertively brand ethical meta-services after fully tested customer service. Completely orchestrate intuitive communities through superior markets. Efficiently leverage other’s out-of-the-box.</p>
+							</div>
+							<div class="col-md-4 content" data-aos="fade-left">
+								<p>Assertively brand ethical meta-services after fully tested customer service. Completely orchestrate intuitive communities through superior markets. Efficiently leverage other’s out-of-the-box.</p>
+							</div>
+						</div>
+					</div>
+
+				</div>
+
+
+				<div class="item-container" data-aos="fade-left">
+			<button class="slider slider--left">
+					<i class="fas fa-angle-left"></i>
+				</button>
+			<div class="items">
+			<div class="item">
+				<img src="img/photo-1517423568366-8b83523034fd.jpg" alt="" class="item__img">
+			<!--      <a href="#" class="item__link">See More</a>    -->
+			</div>
+			<div class="item">
+				<img src="img/jyotirmoy-gupta-fbUvVn1_ViQ-unsplash.jpg" alt="" class="item__img">
+			<!--      <a href="#" class="item__link">See More</a>   -->
+			</div>
+			<div class="item">
+				<img src="img/whoislimos-kFVmYjK6hZ8-unsplash.jpg" alt="" class="item__img">
+			<!--      <a href="#" class="item__link">See More</a>   -->
+			</div>
+			<div class="item">
+				<img src="img/photo-1517423568366-8b83523034fd.jpg" alt="" class="item__img">
+			<!--    <a href="#" class="item__link">See More</a>   -->
+			</div>
+			</div>
+			<button class="slider slider--right">
+						<i class="fas fa-angle-right"></i>
+					</button>
+			</div>
+
+
+				<div class="container" data-aos="fade-right">
+					<div class="row">
+						<div class="col-md-12 para">
+							<p style="font-size:18px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						</div>
+					</div>
+					<hr class="hr">
+				</div>
+				</section>
+
+				<section>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<div class="col-md-7" data-aos="fade-right">
+								<img src="img/px-clouds-conifers-fir-trees-158672-origin.jpg" alt="" style="width:100%;" >
+							</div>
+							<div class="col-md-5" data-aos="fade-left">
+								<div style="clear:both; height:80px;"></div>
+								<h1 style="font-family: 'Fredoka One', cursive; padding-bottom:10px;" ><b>YOUR BIT</b></h1>
+								<p style="font-size:18px;">Assertively brand ethical meta-services after fully tested service customer . Completely orchestrate intuitive are i through  communities  superior markets. Efficiently brand leverage other’s out-of-the-box.</p>
+							</div>
+						</div>
+						<div style="clear:both; height:30px;"></div>
+						<div class="col-md-12" data-aos="fade-left">
+							<div class="col-md-2"></div>
+						<button type="button" class="pdfbtn"><i class='fas fa-mouse-pointer'> &nbsp; If You Are Intrested In Contributing Please Click </i></button>
+							<div class="col-md-2"></div>
+						</div>
+					</div>
+					<hr class="hr">
+				</div>
+				</section>
+
+				<section>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+
+						</div>
+					</div>
+				</div>
+				</section>
+
+					<section>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12" data-aos="fade-right">
+							<h1 style="font-family: 'Fredoka One', cursive; padding-bottom:20px;" ><b>CERTIFICATION</b></h1>
+						</div>
+					</div>
+				</div>
+						<div class="container">
+							<div class="row">
+						<div class="col-md-12 content">
+							<div class="col-md-4" data-aos="fade-right">
+								<p>Assertively brand ethical meta-services after fully tested customer service. Completely orchestrate intuitive communities through superior markets. Efficiently leverage other’s out-of-the-box.</p>
+							</div>
+							<div class="col-md-4" data-aos="fade-up">
+								<p>Assertively brand ethical meta-services after fully tested customer service. Completely orchestrate intuitive communities through superior markets. Efficiently leverage other’s out-of-the-box.</p>
+							</div>
+							<div class="col-md-4" data-aos="fade-left">
+								<p>Assertively brand ethical meta-services after fully tested customer service. <br> <br> <br><button type="button" class="pdfbtn"><i class="fa fa-eye">&nbsp; View PDF</i></button> </p>
+							</div>
+						</div>
+					</div>
+						<hr class="hr">
+				</div>
+				</section>
+
+
+
+				<section>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12" data-aos="fade-right">
+							<img src="img/px-boat-clouds-dawn-2431419-origin.jpg" alt="" style="width:100%;">
+						</div>
+					</div>
+				</div>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+						<div class="col-md-4"></div>
+						<div class="col-md-4" data-aos="fade-left">
+							<img src="img/logo/logo3.png" alt="" style="width:100%; padding:20px;">
+						</div>
+						<div class="col-md-4"></div>
+						</div>
+
+						<div class="col-md-12">
+							<div class="col-md-4 content" data-aos="fade-right">
+								<p>Assertively brand ethical meta-services after fully tested customer service. Completely orchestrate intuitive communities through superior markets. Efficiently leverage other’s out-of-the-box.</p>
+							</div>
+							<div class="col-md-4 content" data-aos="fade-up">
+								<p>Assertively brand ethical meta-services after fully tested customer service. Completely orchestrate intuitive communities through superior markets. Efficiently leverage other’s out-of-the-box.</p>
+							</div>
+							<div class="col-md-4 content" data-aos="fade-left">
+								<p>Assertively brand ethical meta-services after fully tested customer service. Completely orchestrate intuitive communities through superior markets. Efficiently leverage other’s out-of-the-box.</p>
+							</div>
+						</div>
+					</div>
+					<hr class="hr">
+				</div>
+				</section>
+
+				<section>
+					<div class="container foot">
+						<div class="row">
+							<div class="col-md-12">
+									<div class="footer-container">
+										<div class="col-md-3" data-aos="fade-right">
+											<a href="#"> <h2>ABOUT</h2> </a>
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+											<div class="footer-links">
+												<a href="#"><p>EVENTS</p> </a>
+												<a href="#"><p>BLOGS</p> </a>
+												<a href="#"><p>CANDIDATES</p> </a>
+												<a href="#"><p>TESTIMONIALS</p> </a>
+												<a href="#"><p>FORMS</p> </a>
+												<a href="#"><p>GALLERY</p> </a>
+											</div>
+										</div>
+
+
+										<div class="col-md-3" data-aos="fade-left">
+											<a href="#"> <h2>CAFEBLE</h2> </a>
+											<div class="footer-links2">
+												<a href="#"><p>Training</p> </a>
+												<a href="#"><p>Menu</p> </a>
+												<a href="#"><p>Testimonials</p> </a>
+												<a href="#"><p>Events</p> </a>
+											</div>
+										</div>
+
+										<div class="col-md-3" data-aos="fade-right">
+											<a href="#"> <h2>YOUR BIT</h2> </a>
+											<div class="footer-links2">
+												<a href="#"><p>Forms</p> </a>
+												<a href="#"><p>Support</p> </a>
+											</div>
+										</div>
+
+										<div class="col-md-3" data-aos="fade-left">
+											<a href="#"> <h2>CERTIFICATION</h2> </a>
+											<div class="footer-links2">
+												<a href="#"><p>Forms</p> </a>
+												<a href="#"><p>Support</p> </a>
+											</div>
+												<a href="#"> <h2>CLIENTS</h2> </a>
+												<a href="#"> <h2>CONTACT</h2> </a>
+												<a href="#"> <h2>&nbsp;</h2> </a>
+												<div class='demopadding'>
+  <div class='icon social fb'><i class='fa fa-facebook'></i></div>
+  <div class='icon social tw'><i class='fa fa-twitter'></i></div>
+  <div class='icon social in'><i class='fa fa-linkedin'></i></div>
+</div>
+
+										</div>
+									</div>
+							</div>
+						</div>
+					</div>
+				</section>
+
+
+				<script type="text/javascript">
+				var slides = document.querySelectorAll(".slide");
+				var dots = document.querySelectorAll(".dot");
+				var index = 0;
+				function prevSlide(n){
+					index+=n;
+					console.log("prevSlide is called");
+					changeSlide();
+				}
+				function nextSlide(n){
+					index+=n;
+					changeSlide();
+				}
+				changeSlide();
+				function changeSlide(){
+					if(index>slides.length-1)
+						index=0;
+					if(index<0)
+						index=slides.length-1;
+						for(let i=0;i<slides.length;i++){
+							slides[i].style.display = "none";
+							dots[i].classList.remove("active");
+						}
+						slides[index].style.display = "block";
+						dots[index].classList.add("active");
+				}
+				</script>
+				<script type="text/javascript">
+				let leftSlider = document.querySelector(".slider--left");
+				let rightSlider = document.querySelector(".slider--right");
+				let items = document.querySelectorAll(".item");
+
+				let maxClicks = items.length - 3;
+				let numClicks = 0;
+
+				items.forEach((item) => {
+				  item.addEventListener("transitionend", () =>{
+				    item.style.transition = "none";
+				  });
+				})
+
+				leftSlider.addEventListener("click", () => {
+				  if (numClicks != 0) {
+				    items.forEach(item => {
+				      let currLeft = item.style.left ? parseFloat(item.style.left) : 0;
+				      currLeft += GetWidth();
+
+				      item.style.transition = "left 0.3s";
+				      item.style.left = currLeft + "px";
+				    });
+				    numClicks--;
+				  }
+				});
+
+				rightSlider.addEventListener("click", () => {
+				  if (numClicks != maxClicks) {
+				    items.forEach(item => {
+				      let currLeft = item.style.left ? parseFloat(item.style.left) : 0;
+				      currLeft -= GetWidth();
+
+				      item.style.transition = "left 0.3s";
+				      item.style.left = currLeft + "px";
+				    });
+				    numClicks++;
+				  }
+				});
+
+				function GetWidth() {
+				  let item = document.querySelector(".item");
+				  let style = window.getComputedStyle(item);
+
+				  let itemWidth =
+				    parseFloat(item.offsetWidth) +
+				    parseFloat(style.marginLeft) +
+				    parseFloat(style.marginRight);
+
+				  return itemWidth;
+				}
+
+				window.addEventListener("resize", () => {
+				  let newLeft = numClicks * GetWidth();
+
+				  items.forEach((item) => {
+				    item.style.left = - (newLeft) + "px";
+				  });
+				});
+				</script>
+				<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+ <script>
+	 AOS.init();
+ </script>
+	</body>
+</html>
---
+++ portfolio-slider.php
@@ -0,0 +1,107 @@
+<section class="portfolio-section portfolio-slider">
+    <div class="container-fluid">
+        <div class="carousel-outer">
+            <div class="portfolio-carousel default-arrows owl-carousel owl-theme">
+                <!-- Portfolio Block Two -->
+                <div class="portfolio-block-two">
+                    <div class="image-box">
+                        <figure class="image"><a href="#"><img src="images/gallery/slider-1.jpg" alt="" class="portfolio_image"></a></figure>
+                        <div class="hidden_info">
+                            <h5><a href="#">Tellus at urna</a></h5>
+                            <p>
+                                <a href="#">Slider</a> /
+                                <a href="#">Design</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Portfolio Block Two -->
+                <div class="portfolio-block-two">
+                    <div class="image-box">
+                        <figure class="image"><a href="#"><img src="images/gallery/slider-2.jpg" alt="" class="portfolio_image"></a></figure>
+                        <div class="hidden_info">
+                            <h5><a href="#">Tellus at urna</a></h5>
+                            <p>
+                                <a href="#">Slider</a> /
+                                <a href="#">Design</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Portfolio Block Two -->
+                <div class="portfolio-block-two">
+                    <div class="image-box">
+                        <figure class="image"><a href="#"><img src="images/gallery/slider-3.jpg" alt="" class="portfolio_image"></a></figure>
+                        <div class="hidden_info">
+                            <h5><a href="#">Tellus at urna</a></h5>
+                            <p>
+                                <a href="#">Slider</a> /
+                                <a href="#">Design</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Portfolio Block Two -->
+                <div class="portfolio-block-two">
+                    <div class="image-box">
+                        <figure class="image"><a href="#"><img src="images/gallery/slider-4.jpg" alt="" class="portfolio_image"></a></figure>
+                        <div class="hidden_info">
+                            <h5><a href="#">Tellus at urna</a></h5>
+                            <p>
+                                <a href="#">Slider</a> /
+                                <a href="#">Design</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Portfolio Block Two -->
+                <div class="portfolio-block-two">
+                    <div class="image-box">
+                        <figure class="image"><a href="#"><img src="images/gallery/slider-5.jpg" alt="" class="portfolio_image"></a></figure>
+                        <div class="hidden_info">
+                            <h5><a href="#">Tellus at urna</a></h5>
+                            <p>
+                                <a href="#">Slider</a> /
+                                <a href="#">Design</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Portfolio Block Two -->
+                <div class="portfolio-block-two">
+                    <div class="image-box">
+                        <figure class="image"><a href="#"><img src="images/gallery/slider-6.jpg" alt="" class="portfolio_image"></a></figure>
+                        <div class="hidden_info">
+                            <h5><a href="#">Tellus at urna</a></h5>
+                            <p>
+                                <a href="#">Slider</a> /
+                                <a href="#">Design</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Portfolio Block Two -->
+                <div class="portfolio-block-two">
+                    <div class="image-box">
+                        <figure class="image"><a href="#"><img src="images/gallery/slider-7.jpg" alt="" class="portfolio_image"></a></figure>
+                        <div class="hidden_info">
+                            <h5><a href="#">Tellus at urna</a></h5>
+                            <p>
+                                <a href="#">Slider</a> /
+                                <a href="#">Design</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</section>
